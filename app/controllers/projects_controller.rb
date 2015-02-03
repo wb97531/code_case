82db43249fb581245d3c5b830c4be5a71456cda6
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-02-03 15:28:16 -0700

    deleted auto comments from project scaffold in controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,22 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
     @coder = @project.coder
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
     @action = 'edit'
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
     @project.current = true
@@ -39,8 +31,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
@@ -53,8 +43,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
@@ -64,12 +52,10 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       params.require(:project).permit(:project_name, :github_link, :deadline, :description, :priority, :coder_id, :current)
     end
