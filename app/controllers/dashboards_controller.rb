7c62997d8d68f28b6b52360a677d00fff4b7d919
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-03-14 19:08:35 -0600

    [#90343224] changed to set @coder value if not nil

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
   def dashboard
-    @coder = Coder.find(params[:id])
+    @coder ||= Coder.find(params[:id])
     current_coder ? @projects = current_coder.projects : @projects = @coder.projects
     current_coder ? @snippets = current_coder.snippets : @snippets = @coder.snippets
     current_coder ? @courses  = current_coder.courses.order(:completion_date).reverse :
