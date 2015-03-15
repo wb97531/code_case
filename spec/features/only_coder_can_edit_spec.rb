9bd50e406d7e7dd645b5bec717d61d14616d2f82
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-03-14 19:11:03 -0600

    Create Editing Spec
      * created a spec to test only coder could change their own information
    
    [#90343224]

---
+++ spec/features/only_coder_can_edit_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+feature 'Making edits' do
+  scenario "coder can add, edit or delete their own work" do
+    @coder_1 = FactoryGirl.create(:coder, password: '1234567', password_confirmation: '1234567')
+    @project = FactoryGirl.create(:project, coder_id: @coder_1.id)
+    @course = FactoryGirl.create(:course, coder_id: @coder_1.id)
+    @snippet = FactoryGirl.create(:snippet, coder_id: @coder_1.id)
+
+    visit '/'
+
+    fill_in 'Email', with: @coder_1.email
+    fill_in 'Password', with: '1234567'
+
+    click_button 'Login'
+
+    visit "/projects/#{@project.id}/edit"
+
+    expect(page).to_not have_content('Password')
+    expect(page).to have_content('Editing')
+
+    visit "/courses/#{@course.id}/edit"
+
+    expect(page).to_not have_content('Password')
+    expect(page).to have_content('Editing')
+
+    visit "/snippets/#{@snippet.id}/edit"
+
+    expect(page).to_not have_content('Password')
+    expect(page).to have_content('Editing')
+  end
+
+  scenario "coder is redirected to login when trying to add, edit or delete another coder's work" do
+    @coder_1 = FactoryGirl.create(:coder, password: '1234567', password_confirmation: '1234567')
+    @coder_2 = FactoryGirl.create(:coder, password: '1234567', password_confirmation: '1234567')
+    @project = FactoryGirl.create(:project, coder_id: @coder_2.id)
+    @course = FactoryGirl.create(:course, coder_id: @coder_2.id)
+    @snippet = FactoryGirl.create(:snippet, coder_id: @coder_2.id)
+
+    visit '/'
+
+    fill_in 'Email', with: @coder_1.email
+    fill_in 'Password', with: '1234567'
+
+    click_button 'Login'
+
+    visit "/projects/#{@project.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+
+    visit "/courses/#{@course.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+
+    visit "/snippets/#{@snippet.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+  end
+
+
+  scenario "guest is redirected to login when trying to add, edit or delete coder's work" do
+    @coder = FactoryGirl.create(:coder)
+    @project = FactoryGirl.create(:project, coder_id: @coder.id)
+    @course = FactoryGirl.create(:course, coder_id: @coder.id)
+    @snippet = FactoryGirl.create(:snippet, coder_id: @coder.id)
+
+    visit '/login'
+
+    click_link 'Guest'
+
+    fill_in "Enter the Coder's email", with: @coder.email
+
+    click_button "See what they're working on"
+
+    visit "/projects/#{@project.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+
+    visit "/courses/#{@course.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+
+    visit "/snippets/#{@snippet.id}/edit"
+
+    expect(page).to have_content('Password')
+    expect(page).to_not have_content('Editing')
+  end
+end
