38a69ed5ab637f1e725198c84a4bf96af7b83acc
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-03-25 14:45:37 -0600

    Add spec for auto delete snippet
      * added a test to the model spec for snippet that checks if it was automatically deleted when it's associated project was deleted
      * made the test pass and checked that all other tests still passed
    
    [#91121336]

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
   belongs_to :coder
-  has_many :snippets
+  has_many :snippets, dependent: :destroy
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-# RSpec.describe Snippet, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Snippet, type: :model do
+  it 'snippet should be automatically deleted when associated project is deleted' do
+    coder = FactoryGirl.create(:coder, id: 2000)
+    project = FactoryGirl.create(:project, coder_id: coder.id, id: 2000)
+    FactoryGirl.create(:snippet, coder_id: coder.id, project_id: project.id, id: 2000)
+
+    project.destroy!
+
+    expect { Snippet.find(2000) }.to raise_error ActiveRecord::RecordNotFound
+  end
+end
