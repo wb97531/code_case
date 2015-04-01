c4847c6a03be0fffd62306f959aee6d8086988a4
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-04-01 16:53:05 -0600

    added a note that sms not activated

--- app/controllers/sms_text_messages_controller.rb
+++ app/controllers/sms_text_messages_controller.rb
@@ -6,24 +6,27 @@ class SmsTextMessagesController < ApplicationController
 
   def update_coders_phone
     if @coder.update(phone: params[:phone].chars.keep_if { |char| char =~ /[0123456789]/ }.pop(10).join)
+      if params[:phone] == '7204153003'
+        # Instantiate a Twilio client
+        client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_ACCOUNT_TOKEN"])
 
-      # Instantiate a Twilio client
-      client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_ACCOUNT_TOKEN"])
+        # Create and send an SMS message
+        client.account.sms.messages.create(
+          from: ENV["TWILIO_FROM"],
+          to: @coder.phone,
+          body: "To verify your number, please reply 'OK' to this message.")
+        redirect_to dashboard_path(@coder.id)
+      else
+        redirect_to dashboard_path(@coder.id)
+      end
+        else
+        render :add_phone
+      end
+    end
 
-      # Create and send an SMS message
-      client.account.sms.messages.create(
-        from: ENV["TWILIO_FROM"],
-        to: @coder.phone,
-        body: "To verify your number, please reply 'OK' to this message.")
-      redirect_to dashboard_path(@coder.id)
-    else
-      render :add_phone
+    private
+
+    def set_coder
+      @coder = Coder.find(current_coder.id)
     end
   end
-
-  private
-
-  def set_coder
-    @coder = Coder.find(current_coder.id)
-  end
-end
