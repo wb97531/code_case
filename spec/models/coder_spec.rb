d5d08b532a033e19070f0947941ceb44e17806a4
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-05-02 13:14:28 -0600

    create specs to test mailer

--- spec/models/coder_spec.rb
+++ spec/models/coder_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe Coder, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:coder_name) }
   it { should validate_length_of(:password) }
+  it { should have_db_column(:verified_email).with_options(null: true) }
+  it { should have_db_column(:token).with_options(null: true) }
+end
+
+describe 'needs_verification!' do
+  let(:coder) { FactoryGirl.create(:coder, token: '', verified_email: true) }
+
+  it 'sets the verified email address field as false' do
+    expect { coder.needs_verification! }.to change(
+      coder, :verified_email?).from(true).to(false)
+  end
+
+  it 'sets the token for the coder' do
+    expect { coder.needs_verification! }.to change(coder, :token)
+  end
+
+  it 'sends a verification email' do
+    expect { coder.needs_verification! }.to change(
+      ActionMailer::Base.deliveries, :count).by(1)
+  end
 end
