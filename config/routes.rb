380bcb0702cabed0ac1f118ce46e2038a9faa6ff
Wendy Brannon <wbsolutionsllc2@gmail.com>
2015-05-02 13:13:46 -0600

    * add a default url in test * changed default url for production in heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # default_url_options host: 'loaclhost', port: 3000
+  default_url_options host: 'heroku', port: 5000
+
   resources :courses
 
   resources :snippets
@@ -10,14 +13,15 @@ Rails.application.routes.draw do
   resources :verifications
 
   root 'sessions#login'
-  get 'dashboard/:id', to: 'dashboards#dashboard', as: 'dashboard'
-  get '/login', to: 'sessions#login', as: 'get_login'
-  post '/login', to: 'sessions#create', as: 'login'
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
-  get '/guest', to: 'sessions#guest', as: 'guest'
-  post '/view_coder', to: 'sessions#view_coder', as: 'view_coder'
-  get 'email_coder', to: 'dashboards#email_form'
-  post 'send_coder_email', to: 'dashboards#send_coder_email'
-  get 'add_phone', to: 'sms_text_messages#add_phone'
+  get 'dashboard/:id',            to: 'dashboards#dashboard',                  as: 'dashboard'
+  get 'verification/:token',      to: 'coders#verify',                         as: 'verify_email'
+  get 'login',                    to: 'sessions#login',                        as: 'get_login'
+  post 'login',                   to: 'sessions#create',                       as: 'login'
+  delete 'logout',                to: 'sessions#destroy',                      as: 'logout'
+  get 'guest',                    to: 'sessions#guest',                        as: 'guest'
+  post 'view_coder',              to: 'sessions#view_coder',                   as: 'view_coder'
+  get 'email_coder',              to: 'dashboards#email_form'
+  post 'send_coder_email',        to: 'dashboards#send_coder_email'
+  get 'add_phone',                to: 'sms_text_messages#add_phone'
   post 'update_coders_phone/:id', to: 'sms_text_messages#update_coders_phone', as: 'update_coders_phone'
 end
